bc711889af0e041ff2ede05be8d3b3121b95db11
Derrick <derrickhbutler@gmail.com>
2020-04-17 19:27:28 -0400

    initial commit for Javascript homework

---
+++ javascript-challenge.js
@@ -0,0 +1,71 @@
+
+ var date_element = document.getElementById("date");
+
+document.getElementById("Submit").addEventListener("click", displayDate);
+
+function displayDate(e) {
+     e.preventDefault();
+  console.log(date_element.value);
+  console.log(typeof date_element.value);
+     date_array = date_element.value.split("-");
+     var year = date_array[0];
+     var month = parseInt(date_array[1],10);
+     var day = parseInt(date_array[2],10);
+     console.log(month + "/" + day + "/" + year);
++
+
+     var user_date = new Date(year,month,day);
+filterDate(user_date);
+}
+function filterDate(user_date){
+     let  tableData = data;
+//console.log(data); 
+// reference to the html table, where we are going to inject the data - this is the box
+var Table_Box = document.getElementById("Table");
+//create the actual table within the box
+var actual_table = document.createElement("table",  {class : 'table-striped', class : 'table-bordered'});
+//  CREATE Table rows and columns
+var row = document.createElement('tr');
+
+//use to extracts keys and set as table headers
+row.innerHTML = "<th>datetime</th><th>city</th><th>state</th><th>country</th><th>shape</th><th>durationMinutes</th><th>comments</th>";
+actual_table.appendChild(row);
+
+
+
+//var Table_Box;
+for (var i=0; i < tableData.length; i++) {
+     var info = tableData[i]; 
+     if (typeof info == "undefined") continue;
+     console.log(info.datetime);
+    //get date from info
+    var date = info.datetime;
+    //console.log(date)
+   var date_array = date.split("/");
+    var month = parseInt(date_array[0]);
+    var day = parseInt(date_array[1]);
+    var year = parseInt(date_array[2]);
+    console.log(year + " " + month + " " + day);
+    var compareDate = new Date(year,month,day);
+    //compare date to user date
+    if (compareDate > user_date){continue;}
+    else if (user_date > compareDate){continue;}
+    //if NOT > OR < 
+    //add table to row
+console.log(compareDate);
+console.log(user_date);
+
+    
+     var table_row = document.createElement('tr');
+     table_row.innerHTML = "<td>"+ info.datetime +"</td>"+"<td>"+info.city+"</td>"+"<td>"+info.state+"</td>"+"<td>"+info.country+ "</td>"+ info.shape +"</td>"+"<td>"+info.durationMinutes+"</td>"+"<td>"+info.comments+"</td>";
+
+
+actual_table.appendChild(table_row);
+}
+
+Table_Box.innerHTML = "";
+Table_Box.appendChild(actual_table);
+
+}
+
+
